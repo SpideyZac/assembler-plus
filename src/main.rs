c69043324cb96fb5da8f675c32e55d747e85f80d
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2024-08-07 23:40:50 +0000

    feat: lexer, emitter, errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod emit;
+mod error;
+mod lexer;
+mod tokens;
+
+use crate::emit::Emitter;
+use crate::lexer::Lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let code = ".main str r1 r2 0\nhlt".to_string();
+    let emitter = Emitter::new(code.clone(), "out.asm".to_string());
+    let mut lexer = Lexer::new(code);
+    let tokens = lexer.lex();
+    if let Ok(token) = tokens {
+        println!("{:?}", token);
+    } else {
+        let error = tokens.unwrap_err();
+        emitter.emit_error(error);
+        println!("abc");
+    }
 }
