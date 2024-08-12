05d72f65d2a47704a12f14b4590a5dc88b4e03f2
spideyzac <zacmlesser-7@outlook.com>
2024-08-11 23:51:31 -0700

    feat: more templates for codegen

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,7 +1,8 @@
+use crate::lexer::{Mnemonic, TokenKind};
 use crate::parser::{AstStmt, AstStmtOperand, Statement, __token_ast_Token};
 
 use laps::log_error;
-use laps::span::Error;
+use laps::span::{Error, Span};
 
 macro_rules! eval_err {
     ($span:expr, $($arg:tt)+) => {
@@ -27,6 +28,105 @@ impl Codegen {
         Ok(output)
     }
 
+    fn _check_stmt_operand(&mut self, operand: AstStmtOperand, correct: Vec<&str>) -> bool {
+        match operand {
+            AstStmtOperand::Register(_) => {
+                if !correct.contains(&"register") {
+                    return false;
+                }
+            }
+            AstStmtOperand::Label(_) => {
+                if !correct.contains(&"label") {
+                    return false;
+                }
+            }
+            AstStmtOperand::Int(_) => {
+                if !correct.contains(&"int") {
+                    return false;
+                }
+            }
+            AstStmtOperand::Identifier(_) => {
+                if !correct.contains(&"identifier") {
+                    return false;
+                }
+            }
+            AstStmtOperand::Char(_) => {
+                if !correct.contains(&"char") {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
+    fn check_stmt_operands(
+        &mut self,
+        span: Span,
+        operands: Vec<AstStmtOperand>,
+        correct: Vec<Vec<&str>>,
+    ) -> Result<(), Error> {
+        if operands.len() != correct.len() {
+            eval_err!(
+                span,
+                "expected {} operands, got {}",
+                correct.len(),
+                operands.len()
+            )
+        }
+
+        for (i, operand) in operands.iter().enumerate() {
+            let expected = correct[i].clone();
+            match operand {
+                AstStmtOperand::Register(r) => {
+                    if !expected.contains(&"register") {
+                        eval_err!(
+                            r.0.span.clone(),
+                            "expected {} operand, got register",
+                            expected.join(" or ")
+                        )
+                    }
+                }
+                AstStmtOperand::Label(l) => {
+                    if !expected.contains(&"label") {
+                        eval_err!(
+                            l.0.span.clone(),
+                            "expected {} operand, got label",
+                            expected.join(" or ")
+                        )
+                    }
+                }
+                AstStmtOperand::Int(i) => {
+                    if !expected.contains(&"int") {
+                        eval_err!(
+                            i.0.span.clone(),
+                            "expected {} operand, got int",
+                            expected.join(" or ")
+                        )
+                    }
+                }
+                AstStmtOperand::Identifier(ident) => {
+                    if !expected.contains(&"identifier") {
+                        eval_err!(
+                            ident.0.span.clone(),
+                            "expected {} operand, got identifier",
+                            expected.join(" or ")
+                        )
+                    }
+                }
+                AstStmtOperand::Char(c) => {
+                    if !expected.contains(&"char") {
+                        eval_err!(
+                            c.0.span.clone(),
+                            "expected {} operand, got char",
+                            expected.join(" or ")
+                        )
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
     fn generate_stmt(&mut self, stmt: Statement) -> Result<String, Error> {
         match stmt {
             Statement::AstStmt(ast) => {
@@ -45,7 +145,21 @@ impl Codegen {
     fn generate_ast_stmt(&mut self, ast: AstStmt) -> Result<String, Error> {
         let mnemonic = ast.mnemonic.0;
         let operands = ast.operands;
-        eval_err!(mnemonic.span.clone(), "mnemonic not supported")
+
+        if let TokenKind::Mnemonic(m) = mnemonic.kind {
+            match m {
+                Mnemonic::Define => {
+                    self.check_stmt_operands(
+                        mnemonic.span.clone(),
+                        operands.clone(),
+                        vec![vec!["identifier"], vec!["int", "char"]],
+                    )?;
+                    return Ok(format!("{:?} {:?}\n", operands[0], operands[1]));
+                }
+                _ => eval_err!(mnemonic.span.clone(), "mnemonic not supported"),
+            }
+        }
+        panic!("unreachable")
     }
 
     fn generate_label_decl(&mut self, label: __token_ast_Token::Token3) -> Result<String, Error> {
